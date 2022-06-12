42d833cf6cdd59c7345eb91b8a226b2ba656b747
u4iha312@mail.ru <u4iha312@mail.ru>
2022-06-12 03:23:34 +0300

    Add code formating

--- test-app.js
+++ test-app.js
@@ -1,11 +1,11 @@
 console.log("Hello!");
 
-let word ="world";
+let word = "world";
 console.log(word);
 
 const user = {
-  name: "Alex", 
-  age: 34, 
+  name: "Alex",
+  age: 34,
   isEmployee: true
 }
 console.log(user);
